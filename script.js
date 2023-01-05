82558548f6f7076d535e2028c2589be09fe44bdd
YSM-0 <120345032+YSM-0@users.noreply.github.com>
2023-01-05 17:25:37 +0100

    added a button to prompt how much square per side to draw and an hover style on each squares

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // SCRIPT OF ETCH A SKETCH
 
 function createGrid() {
-    const mainContainer = document.querySelector('.main-container');
+    const gridContainer = document.querySelector('.grid-container');
     const squaresInRowNumber =  Math.floor(window.innerWidth/16);
     
     for (let i = squaresInRowNumber; i >= 0; i--) {
         const row = document.createElement('div');
 
-        mainContainer.appendChild(row);
+        gridContainer.appendChild(row);
         row.classList.add('gridRow');
 
         for (let i = squaresInRowNumber; i >= 0; i--) { 
@@ -21,3 +21,5 @@ function createGrid() {
 }
 
 createGrid();
+
+document.querySelector('.size-button').addEventListener('click', () => prompt('How much squares per side do you want to draw?', ''));
